2dcace0ffd875d5f13d26a2eb1091c08031c4d20
seongmin221 <seongmin221@naver.com>
2022-07-10 21:08:50 +0900

    [Feat] editing voicemail Model in progress

--- liveOnReboot/liveOn/Model/Gift/VoiceMail/VoicemailModel.swift
+++ liveOnReboot/liveOn/Model/Gift/VoiceMail/VoicemailModel.swift
@@ -9,5 +9,11 @@ import Foundation
 
 struct Voicemail: Codable {
     
-    var 
+    let createdAt: String
+    let giftVoiceMailDuration: String
+    let title: String
+    var userNickName: String
+    var voiceMail: String
+    var voiceMailId: Int
+
 }
