32ccf3071b534d746b45976b5a8c9bd150f6d45b
italsehf <alsehfdl0316@naver.com>
2022-07-10 18:09:53 +0900

    [Setting] View 폴더에 각각의 선물 보내기 View를 포함하고 있는 SendGift 폴더 추가

---
+++ liveOnReboot/liveOn/View/SendGift/SendLetter/SendLetterView.swift
@@ -0,0 +1,20 @@
+//
+//  SendLetterView.swift
+//  liveOnReboot
+//
+//  Created by Keum MinSeok on 2022/07/10.
+//
+
+import SwiftUI
+
+struct SendLetterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SendLetterView_Previews: PreviewProvider {
+    static var previews: some View {
+        SendLetterView()
+    }
+}
---
+++ liveOnReboot/liveOn/View/SendGift/SendVoiceMail/SendVoiceMailView.swift
@@ -0,0 +1,20 @@
+//
+//  SendVoiceMailView.swift
+//  liveOnReboot
+//
+//  Created by Keum MinSeok on 2022/07/10.
+//
+
+import SwiftUI
+
+struct SendVoiceMailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SendVoiceMailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SendVoiceMailView()
+    }
+}
