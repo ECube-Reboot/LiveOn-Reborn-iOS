cb4f608f123f6b7160fabbed85eb68e2d2b08777
seongmin221 <seongmin221@naver.com>
2022-07-10 20:50:45 +0900

    [Feat] added voicemail ViewModel & Model

--- liveOnReboot/liveOn/Model/Gift/VoiceMail/VoicemailModel.swift
+++ liveOnReboot/liveOn/Model/Gift/VoiceMail/VoicemailModel.swift
@@ -1,20 +1,13 @@
 //
-//  VoiceMailModel.swift
+//  VoicemailModel.swift
 //  liveOnReboot
 //
-//  Created by Keum MinSeok on 2022/07/09.
+//  Created by 이성민 on 2022/07/10.
 //
 
-import SwiftUI
+import Foundation
 
-struct VoiceMailModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct VoiceMailModel_Previews: PreviewProvider {
-    static var previews: some View {
-        VoiceMailModel()
-    }
+struct Voicemail: Codable {
+    
+    var 
 }
