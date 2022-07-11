d76eb6553183d672646a1712b0bd007446f54b80
Milli <jinccc72@pusan.ac.kr>
2022-07-11 22:38:48 +0900

    [Feat] Color, Font Extension 생성

---
+++ liveOnReboot/liveOn/Extensions/Font+Extension.swift
@@ -0,0 +1,38 @@
+//
+//  Font+Extension.swift
+//  liveOnReboot
+//
+//  Created by Jineeee on 2022/07/11.
+//
+
+import Foundation
+import SwiftUI
+
+// 사용 예시 : Text("리본").font(.TextStyles.largeCalendarNumber)
+
+enum liveOnFonts: String {
+    // 캘린더뷰에 사용되는 월,년 표시용 폰트
+    case calendarNumber = "NerkoOne-Regular"
+    // 편지, 사진뷰에 사용되는 손글씨 폰트
+    case handWritten = "나눔손글씨 또박또박"
+}
+
+extension Font {
+    enum TextStyles {
+        // 캘린더뷰에 사용되는 월,년 표시용 폰트
+        static var largeCalendarNumber: Font { Font.liveOnFont(weight: .calendarNumber, size: 60.0)}
+        static var mediumCalendarNumber: Font { Font.liveOnFont(weight: .calendarNumber, size: 32.0)}
+        static var smallCalendarNumber: Font { Font.liveOnFont(weight: .calendarNumber, size: 24.0)}
+        
+        // 편지용 손글씨 폰트
+        static var handWrittenTitle: Font { Font.liveOnFont(weight: .calendarNumber, size: 32.0)}
+        static var handWrittenBody: Font { Font.liveOnFont(weight: .calendarNumber, size: 26.0)}
+        static var handWrittenCallout: Font { Font.liveOnFont(weight: .calendarNumber, size: 24.0)}
+    }
+}
+
+private extension Font {
+    static func liveOnFont(weight: liveOnFonts, size: CGFloat) -> Font {
+        Font.custom(weight.rawValue, size: size)
+    }
+}
