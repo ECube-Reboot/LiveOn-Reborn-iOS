77d20988ecb9a140e9da2fdbee92fbbc9a24cef4
Rookie0031 <jisu7408@gmail.com>
2022-07-19 14:48:29 +0900

    [Refactor] Extension 및 Util 관련 코드 이전

--- liveOnReboot/liveOn/View/DeliveryView.swift
+++ liveOnReboot/liveOn/View/DeliveryView.swift
@@ -16,24 +16,18 @@ struct GiftDeliveryView: View {
     var body: some View {
         
         ZStack {
-            
             VStack(spacing: 20) {
-                
-                // TODO: 스케치 이미지 빼고 제대로 된 일러스트 작업해서 넣기
                 Image("successMatching")
                     .resizable()
                     .frame(width: 240, height: 260)
                 
                 VStack {
-                    
                     Text("상대방에게 선물이 배송되었어요!")
                         .font(.system(size: 18))
                         .foregroundColor(.mainBrown)
                         .padding(.vertical)
-                    
                     Button {
                         gotoMain = false
-
                     } label: {
                         Text("보관함으로 돌아가기")
                             .foregroundColor(.white)
@@ -43,15 +37,13 @@ struct GiftDeliveryView: View {
                             .background(Color.crimson)
                             .cornerRadius(12)
                     }
-                    
-                } // VStack
-            } // VStack
-        } // ZStack
+                }
+            }
+        }
         .navigationTitle("배송완료")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden(true)
         .background(.background)
         .ignoresSafeArea()
-        
-    } // body
+    }
 }
