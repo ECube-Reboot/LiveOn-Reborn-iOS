9f2f152cd560d140baf8127c4e50789da8695b5e
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-07-25 22:39:06 +0900

    [Refactor] Flower Model 파일을 asset 기반으로 수정

--- liveOnReboot/liveOn/Model/Gift/Flower/FlowerModel.swift
+++ liveOnReboot/liveOn/Model/Gift/Flower/FlowerModel.swift
@@ -8,9 +8,18 @@
 import Foundation
 
 enum Flower {
-    case rose
-    case sunflower
-    case dandelion
+    
+    case angae
+    // 안개꽃
+    case freesia
+    // 프리지아
+    case keum
+    // 금목서
+    case larkspur
+    // 수국
+    case lisianthius
+    // 리시안셔스
+    
 }
 
 class FlowerGift: Identifiable, ObservableObject {
@@ -33,8 +42,24 @@ class FlowerGift: Identifiable, ObservableObject {
     }
 }
 
-var sampleData: [FlowerGift] = [
-    FlowerGift(flower: Flower.rose, createdDate: "2022-07-10", writer: "재헌", message: "엄지 손가락으로 장미꽃을 피워"),
-    FlowerGift(flower: Flower.rose, createdDate: "2022-07-10", writer: "재헌", message: "엄지 손가락으로 장미꽃을 피워"),
-    FlowerGift(flower: Flower.rose, createdDate: "2022-07-10", writer: "재헌", message: "엄지 손가락으로 장미꽃을 피워")
-]
+#if DEBUG
+extension FlowerGift {
+    
+    static func dummyData() -> [FlowerGift] {
+        
+        return [
+            FlowerGift(flower: Flower.angae,
+                       createdDate: "2022-07-10",
+                       writer: "재헌",
+                       message: "엄지 손가락으로 장미꽃을 피워"),
+            FlowerGift(flower: Flower.angae,
+                       createdDate: "2022-07-10",
+                       writer: "재헌",
+                       message: "엄지 손가락으로 장미꽃을 피워"),
+            FlowerGift(flower: Flower.angae,
+                       createdDate: "2022-07-10",
+                       writer: "재헌",
+                       message: "엄지 손가락으로 장미꽃을 피워")]
+    }
+}
+#endif
