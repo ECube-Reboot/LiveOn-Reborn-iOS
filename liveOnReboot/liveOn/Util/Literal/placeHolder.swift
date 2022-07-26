21776c86de87e2fcc0412f4135ff64afa7b059e4
Milli <jinccc72@pusan.ac.kr>
2022-07-26 21:17:55 +0900

    [Add] Literals 추가
    글자제한수, placeholder 등을 쉽게 수정할 수 있도록 따로 뺐습니다!

---
+++ liveOnReboot/liveOn/Util/Literal/placeHolder.swift
@@ -0,0 +1,12 @@
+//
+//  placeHolder.swift
+//  liveOnReboot
+//
+//  Created by Jineeee on 2022/07/26.
+//
+
+import Foundation
+enum PlaceHolder {
+    static var createLetter: String { return "오늘은 어떤 이야기를 해볼까요?"}
+    
+}
---
+++ liveOnReboot/liveOn/Util/Literal/textLimit.swift
@@ -0,0 +1,12 @@
+//
+//  textLimit.swift
+//  liveOnReboot
+//
+//  Created by Jineeee on 2022/07/26.
+//
+
+import Foundation
+enum textLimit {
+  static var nickName: Int { return 10 }
+  static var letterGift: Int { return 10 }
+}
