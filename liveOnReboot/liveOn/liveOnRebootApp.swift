952f536324ed59edb50309f35978aa101f1da65a
Milli <jinccc72@pusan.ac.kr>
2022-08-07 20:10:34 +0900

    [Feat] 쪽지 보내기 후 선물함 이동하기

--- liveOnReboot/liveOn/liveOnRebootApp.swift
+++ liveOnReboot/liveOn/liveOnRebootApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct liveOnRebootApp: App {
+  //테스트용 임시
+  @State var tempGotoMain = false
     var body: some Scene {
         WindowGroup {
-            LetterView()
+            SendLetterView(gotoMain: $tempGotoMain)
         }
     }
+  
+  init() {
+    UITextView.appearance().backgroundColor = .clear
+  }
 }
