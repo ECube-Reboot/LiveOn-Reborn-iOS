e45292d555de8ddabb40ed422a63140b95c02752
Rookie0031 <jisu7408@gmail.com>
2022-08-19 19:32:59 +0900

    fix: PR 리뷰사항 반영

--- liveOnReboot/liveOn/View/SendGift/SendPicture/SendPictureView.swift
+++ liveOnReboot/liveOn/View/SendGift/SendPicture/SendPictureView.swift
@@ -7,14 +7,13 @@
 import SwiftUI
 
 struct SendPictureView: View {
-    @StateObject var pictureModel = PictureViewModel()
     @Environment(\.dismiss) private var dismiss
+    @StateObject var pictureModel = PictureViewModel()
     @State private var comment: String = ""
     @State private var showAlertforSend: Bool = false
     @State private var isSent = false
     @State private var showLoading = false
     @State private var loadingState: Int = 0
-
     @Binding var gotoMain: Bool
 
     var body: some View {
@@ -26,8 +25,8 @@ struct SendPictureView: View {
                             pictureModel.source = .library
                             pictureModel.showPhotoPicker()
                         } label: {
-                            if let image = pictureModel.image {
-                                Image(uiImage: image)
+                            if let selectedImage = pictureModel.image {
+                                Image(uiImage: selectedImage)
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.width * 0.8)
@@ -73,9 +72,9 @@ struct SendPictureView: View {
                         }
                         ToolbarItem(placement: .navigationBarTrailing) {
                             Button {
-                              if pictureModel.image != nil {
-                                showAlertforSend.toggle()
-                              }
+                                if pictureModel.image != nil {
+                                    showAlertforSend.toggle()
+                                }
                             } label: {
                                 Text("선물하기")
                             }
@@ -83,12 +82,12 @@ struct SendPictureView: View {
                             .alert(isPresented: $showAlertforSend) {
                                 Alert(title: Text("선물하기"), message: Text("선물은 하루에 하나만 보낼 수 있어요. 쪽지를 보낼까요?"), primaryButton: .cancel(Text("취소")), secondaryButton: Alert.Button.default(Text("보내기")) {
                                     showLoading.toggle()
-                                  
-                                  imagePost {
-                                    print("전송완료!")
-                                    showLoading.toggle()
-                                    isSent.toggle()
-                                  }
+
+                                    imagePost {
+                                        print("전송완료!")
+                                        showLoading.toggle()
+                                        isSent.toggle()
+                                    }
                                 })
                             }
                         }
@@ -104,8 +103,6 @@ struct SendPictureView: View {
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(Color.background)
-                    //.blur(radius: showLoading ? 6 : 0)
-                    
                 }
                 .padding()
                 .padding(.top, 80)
@@ -114,34 +111,34 @@ struct SendPictureView: View {
                 }
             }
             .overlay{
-              if showLoading {
-                loadingView()
-              }
+                if showLoading {
+                    LoadingView()
+                }
             }
         }
         .frame(maxWidth:.infinity, maxHeight: .infinity)
         .background(Color.background)
     }
 
-  
-  private func imagePost(completion: @escaping () -> ()) {
+
+    private func imagePost(completion: @escaping () -> ()) {
         moyaService.request(.imagePost(comment: comment, polaroid: pictureModel.image ?? UIImage())) { response in
             switch response {
-            case .success(_):
+            case .success:
                 completion()
             case .failure(let err):
                 print(err.localizedDescription)
                 print("========= 통신 자체가 실패했습니다. ========")
             }
         }
-      
+
     }
 }
 
-struct loadingView: View {
-  var body: some View {
-      Text("선물 보내는 중...")
-      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-      .background(.thickMaterial)
-  }
+struct LoadingView: View {
+    var body: some View {
+        Text("선물 보내는 중...")
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .background(.thickMaterial)
+    }
 }
