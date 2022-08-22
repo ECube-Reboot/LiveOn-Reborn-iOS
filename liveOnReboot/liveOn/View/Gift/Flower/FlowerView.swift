6a1b1ac6c6b9c13d76906d4f1b65b29e3acb2efc
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-08-22 21:54:06 +0900

    [Feat] 꽃 선물 보내기 뷰 UI 구현

--- liveOnReboot/liveOn/View/Gift/Flower/FlowerView.swift
+++ liveOnReboot/liveOn/View/Gift/Flower/FlowerView.swift
@@ -11,14 +11,28 @@ import SwiftUI
 struct FlowerView: View {
     
     @Environment(\.dismiss) var dismiss
-        
+    
     var body: some View {
         
         VStack {
-            Text("주고 받은 꽃들을 볼 수 있는 뷰")
+            Text("꽃을 눌러 상세를 볼 수 있어요.")
+            Spacer()
+            
+            // TODO: 사용자가 주고 받은 꽃들을 ZStack과 Grid를 이용해 표시
+            // TODO: 빈 꽃병 에셋 추가
+            
+            Image("flower")
+                .padding(.horizontal, 12)
+            
+            Spacer()
+            Text("더 이전의 꽃은\n달력에서 확인할 수 있어요.")
+                .multilineTextAlignment(.center)
+            Spacer()
             
         } // VStack
         .navigationToBack(dismiss)
+        .navigationTitle("꽃")
+        .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
                 NavigationLink(destination: SendFlowerView()) {
@@ -29,7 +43,8 @@ struct FlowerView: View {
                 }
             }
         }
-    }
+        
+    } // body
 }
 
 struct FlowerView_Previews: PreviewProvider {
