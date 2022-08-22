abacb731f78b34a328879815a6ee3baec4ff8856
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-08-22 23:22:48 +0900

    [Feat] 선물 제작 리스트 뷰 UI 구현

--- liveOnReboot/liveOn/View/GiftList/GiftListView.swift
+++ liveOnReboot/liveOn/View/GiftList/GiftListView.swift
@@ -9,24 +9,53 @@ import SwiftUI
 
 struct GiftListView: View {
     
+    // 테스트용 임시
+    @State var tempGotoMain = false
+        
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
         
-        NavigationView{
-            VStack {
-                Image("cassetteIcon04")
-                
-                ZStack {
+        LazyVStack(alignment: .leading, spacing: 6) {
+            Image("cassetteIcon04")
+                .resizable()
+                .frame(width: UIScreen.main.bounds.width * 0.3, height: UIScreen.main.bounds.width * 0.3)
+                .scaledToFill()
+                .offset(x: UIScreen.main.bounds.width * 0.04)
+            
+            ZStack {
                 Image("bubble")
-                    Text("오늘은 어떤 선물을 보내볼까요?")
-                        
+                    .resizable()
+                    .frame(width: UIScreen.main.bounds.width * 0.68, height: UIScreen.main.bounds.width * 0.24)
+                Text("오늘은 어떤 선물을 보내볼까요?")
+                    .offset(y: 4)
+            } // ZStack
+            
+            LazyVStack(alignment: .trailing, spacing: 8) {
+                
+                NavigationLink(destination: SendLetterView(gotoMain: $tempGotoMain)) {
+                    CardWithStroke(cardName: "쪽지", cardImage: "letterIcon")
+                }
+                NavigationLink(destination: PhotoGiftView(gotoMain: $tempGotoMain)) {
+                    CardWithStroke(cardName: "폴라로이드", cardImage: "photocardIcon")
+                }
+                NavigationLink(destination: SendVoiceMailView()) {
+                    CardWithStroke(cardName: "음성메세지", cardImage: "casetteIcon")
+                }
+                NavigationLink(destination: SendFlowerView()) {
+                    CardWithStroke(cardName: "꽃", cardImage: "flowerIcon")
                 }
                 
-            }
-            .navigationToBack(dismiss)
+            } // VStack 우측 제작뷰 이동 버튼 스택
+            .padding(.top, 12)
             
-        } // NavigationView
+        } // VStack
+        .font(.TextStyles.handWrittenBody)
+        .foregroundColor(.textBodyColor)
+        .navigationTitle("선물 목록")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationToBack(dismiss)
+        .padding(.horizontal)
     } // body
 }
 
@@ -37,18 +66,31 @@ struct CardWithStroke: View {
     
     var body: some View {
         
-        ZStack{
-            RoundedRectangle(cornerRadius: 12)
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
                 .stroke()
-                .frame(width: 360, height: 160, alignment: .center)
-            Text(cardName)
+                .foregroundColor(Color.placeHolderColor)
+                .frame(width: UIScreen.main.bounds.width * 0.48, height: UIScreen.main.bounds.height * 0.1, alignment: .center)
+                .background(.white)
             
+
+                HStack {
+                    Image(cardImage)
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width * 0.2, height: UIScreen.main.bounds.width * 0.2, alignment: .center)
+                        .offset(y: 0)
+                    
+                    Text(cardName)
+                        .multilineTextAlignment(.leading)
+            } // HStack
         } // ZStack
     } // body
 }
 
 struct GiftListView_Previews: PreviewProvider {
     static var previews: some View {
+        
         GiftListView()
+        
     }
 }
