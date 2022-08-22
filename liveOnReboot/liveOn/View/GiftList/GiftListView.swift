4f5374a190587991742e8827f30e4f59e2e13c13
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-08-22 22:25:36 +0900

    [Feat] 선물 제작 리스트 뷰에 뒤로가기 기능 추가

--- liveOnReboot/liveOn/View/GiftList/GiftListView.swift
+++ liveOnReboot/liveOn/View/GiftList/GiftListView.swift
@@ -2,15 +2,41 @@
 //  GiftListView.swift
 //  liveOnReboot
 //
-//  Created by Keum MinSeok on 2022/07/09.
+//  Created by teethemoji on 2022/07/09.
 //
 
 import SwiftUI
 
 struct GiftListView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+        NavigationView{
+            VStack {
+                Image("cassetteIcon04")
+            }
+            .navigationToBack(dismiss)
+        } // NavigationView
+    } // body
+}
+
+struct TempCard: View {
+    
+    let cardName: String
+    let
+    
+    var body: some View {
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 12)
+                .stroke()
+                .frame(width: 360, height: 160, alignment: .center)
+            Text(cardName)
+            
+        } // ZStack
+    } // body
 }
 
 struct GiftListView_Previews: PreviewProvider {
