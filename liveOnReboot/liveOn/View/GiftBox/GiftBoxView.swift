5b05b9fb82be57a18fdffe15746022fa203eed67
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-08-22 21:05:36 +0900

    [Comment] 상단 정렬 시도 중간 저장

--- liveOnReboot/liveOn/View/GiftBox/GiftBoxView.swift
+++ liveOnReboot/liveOn/View/GiftBox/GiftBoxView.swift
@@ -2,7 +2,7 @@
 //  GiftBoxView.swift
 //  liveOnReboot
 //
-//  Created by Boyoung Kim on 2022/07/09.
+//  Created by teethemoji on 2022/07/09.
 //
 
 import SwiftUI
@@ -11,41 +11,51 @@ struct GiftBoxView: View {
     var body: some View {
         
         NavigationView {
-            VStack (alignment: .leading, spacing: 12) {
-                HStack {
-                    CoupleInfoLabel()
-                    Spacer()
-                    NavigationLink(destination: GiftListView()) {
-                        Image("addButton")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 34, height: 34, alignment: .center)
+            HStack(alignment: .top, spacing: 12) {
+                VStack (alignment: .center, spacing: 12) {
+                    
+                    // MARK: 커플 정보 라벨과 선물 제작 버튼이 들어가는 상단 바
+                    HStack {
+                        CoupleInfoLabel()
+                        Spacer()
+                        NavigationLink(destination: GiftListView()) {
+                            Image("addButton")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 34, height: 34, alignment: .center)
+                        }
                     }
-                }
-                
-                CalendarLinkView()
-                
-                // TODO: 가운데 정렬
-                /*
-                 
-                 캘린더는 가운데에 잘 붙어 있는데,
-                 쪽지랑 사진, 음성, 꽃들은 가운데에 안 붙고 왼쪽 정렬되어 있음
-                 
-                 */
-                
-                VStack {
-                    LetterAndPictureLinkView()
-                        .padding(.bottom, 12)
-                    VoiceAndFlowerLinkView()
+                    
+                    CalendarLinkView()
+                    
+                    // TODO: 가운데 정렬
+                    /*
+                     
+                     캘린더는 가운데에 잘 붙어 있는데,
+                     쪽지랑 사진, 음성, 꽃들은 가운데에 안 붙고 왼쪽 정렬되어 있음
+                     
+                     그리고 이 모든 것이 상단 정렬되어 있지 않음.. ^^
+                     
+                     */
+                    
+                    VStack(alignment: .center) {
+                        
+                        LetterAndPictureLinkView()
+                            .padding(.bottom, 12)
+                        VoiceAndFlowerLinkView()
+                        
+                        Spacer()
+                    } // VStack
+                    
                 } // VStack
+                .padding(.horizontal)
+                .foregroundColor(.textBodyColor)
                 
-            } // VStack
-            .padding(.horizontal)
-            .foregroundColor(.textBodyColor)
+            } // HStack
             
         } // NavigationView
+        .navigationBarHidden(true)
     } // body
-    
 }
 
 struct CoupleInfoLabel: View {
@@ -157,7 +167,7 @@ struct VoiceAndFlowerLinkView: View {
                     .aspectRatio(contentMode: .fit)
             }
         } // HStack
-    }
+    } // body
 }
 
 struct GiftBoxView_Previews: PreviewProvider {
