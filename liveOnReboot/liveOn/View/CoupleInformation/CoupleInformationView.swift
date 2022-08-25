ce0c30a92d0d812e229b2204bae881c9e1a8b3d0
Milli <jinccc72@pusan.ac.kr>
2022-08-25 17:04:21 +0900

    [Feat] 커플 정보 뷰

--- liveOnReboot/liveOn/View/CoupleInformation/CoupleInformationView.swift
+++ liveOnReboot/liveOn/View/CoupleInformation/CoupleInformationView.swift
@@ -8,10 +8,76 @@
 import SwiftUI
 
 struct CoupleInformationView: View {
+    @State private var showChangeNickNameAlert = false
+    @State private var showChangeOfficialDayAlert = false
+    @State private var changeNickNameInput = "재헌"
+    @State private var officialDate = Date.now
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                List {
+                    NavigationLink(destination: ChangeNickNameView(nickNameTemp: $changeNickNameInput)){
+                        HStack {
+                        Text("내 이름")
+                                .font(.TextStyles.handWrittenBody)
+                        Spacer()
+                            Text(changeNickNameInput)
+                                .font(.TextStyles.handWrittenCallout)
+                                .opacity(0.8)
+                        }
+                        .padding(.vertical, 12)
+                    }
+                    .foregroundColor(.textBodyColor)
+                    .listRowBackground(Color.clear)
+                    .listRowSeparator(.hidden)
+                    
+                    HStack {
+                        Text("상대")
+                                .font(.TextStyles.handWrittenBody)
+                        Spacer()
+                            Text("유진")
+                                .font(.TextStyles.handWrittenCallout)
+                                .opacity(0.8)
+                    }
+                    .padding(.vertical, 12)
+                    .foregroundColor(.textBodyColor)
+                    .listRowBackground(Color.clear)
+                    .listRowSeparator(.hidden)
+                    NavigationLink(destination: ChangeOfficialDateView(officialDay: $officialDate)){
+                        HStack {
+                        Text("우리의 1일")
+                                .font(.TextStyles.handWrittenBody)
+                        Spacer()
+                            Text(officialDate.toString(dateFormat: "YYYY년 M월 d일"))
+                                .font(.TextStyles.handWrittenCallout)
+                                .opacity(0.8)
+                        }
+                        .padding(.vertical, 12)
+                    }
+                    .listRowBackground(Color.clear)
+                    .foregroundColor(.textBodyColor)
+                    .listRowSeparator(.hidden)
+                }
+        .listRowBackground(Color.clear)
+        .navigationTitle("커플정보")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink(destination: SettingView()) {
+                    Image(systemName: "gearshape")
+                }
+            }
+        }
+        .listStyle(.plain)
+        .background(Color.backgroundGray)
+    }
+    
+    init(){
+        UITableView.appearance().backgroundColor = .clear
+        UITableViewCell.appearance().backgroundColor = .red
+        UITableViewCell.appearance().backgroundView?.backgroundColor = .blue
+        UINavigationBarAppearance().backgroundColor = .clear
     }
 }
+
 
 struct CoupleInformationView_Previews: PreviewProvider {
     static var previews: some View {
