6b06d33dde7d9c7e9c7ea96cb4a49406057f2cfb
Rookie0031 <jisu7408@gmail.com>
2022-09-06 18:17:22 +0900

    [Fix] Revert Merege 복원작업

--- liveOnReboot/liveOn/View/GiftList/GiftListView.swift
+++ liveOnReboot/liveOn/View/GiftList/GiftListView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct GiftListView: View {
-    
+
     // 테스트용 임시
     @State var tempGotoMain = false
-        
+
     @Environment(\.dismiss) var dismiss
-    
+
     var body: some View {
-        
+
         LazyVStack(alignment: .leading, spacing: 6) {
             Image("cassetteIcon04")
                 .resizable()
                 .frame(width: UIScreen.main.bounds.width * 0.3, height: UIScreen.main.bounds.width * 0.3)
                 .scaledToFill()
                 .offset(x: UIScreen.main.bounds.width * 0.04)
-            
+
             ZStack {
                 Image("bubble")
                     .resizable()
@@ -30,11 +30,11 @@ struct GiftListView: View {
                 Text("오늘은 어떤 선물을 보내볼까요?")
                     .offset(y: 4)
             } // ZStack
-            
+
             LazyVStack(alignment: .trailing, spacing: 16) {
-                
+
                 // TODO: 카드 내 이미지와 텍스트 왼쪽 정렬
-                
+
                 NavigationLink(destination: SendLetterView(gotoMain: $tempGotoMain)) {
                     CardWithStroke(cardName: "쪽지", cardImage: "letterIcon")
                 }
@@ -44,15 +44,15 @@ struct GiftListView: View {
                 NavigationLink(destination: SendVoiceMailView()) {
                     CardWithStroke(cardName: "음성메세지", cardImage: "casetteIcon")
                 }
-                NavigationLink(destination: SendFlowerView()) {
+                NavigationLink(destination: SendFlowerView(gotoMain: $tempGotoMain)) {
                     CardWithStroke(cardName: "꽃", cardImage: "flowerIcon")
                 }
-                
+
             } // VStack 우측 제작뷰 이동 버튼 스택
             .padding(.top, 12)
-            
+
             Spacer()
-            
+
         } // VStack
         .font(.TextStyles.handWrittenBody)
         .foregroundColor(.textBodyColor)
@@ -60,31 +60,31 @@ struct GiftListView: View {
         .navigationBarTitleDisplayMode(.inline)
         .navigationToBack(dismiss)
         .padding(.horizontal)
-        
+
     } // body
 }
 
 struct CardWithStroke: View {
-    
+
     let cardName: String
     let cardImage: String
-    
+
     var body: some View {
-        
+
         ZStack {
             RoundedRectangle(cornerRadius: 20)
                 .stroke()
                 .foregroundColor(Color.placeHolderColor)
                 .frame(width: UIScreen.main.bounds.width * 0.48, height: UIScreen.main.bounds.height * 0.1, alignment: .center)
                 .background(.white)
-            
+
 
                 HStack {
                     Image(cardImage)
                         .resizable()
                         .frame(width: UIScreen.main.bounds.width * 0.2, height: UIScreen.main.bounds.width * 0.2, alignment: .center)
                         .offset(y: 0)
-                    
+
                     Text(cardName)
                         .multilineTextAlignment(.leading)
             } // HStack
@@ -94,8 +94,8 @@ struct CardWithStroke: View {
 
 struct GiftListView_Previews: PreviewProvider {
     static var previews: some View {
-        
+
         GiftListView()
-        
+
     }
 }
