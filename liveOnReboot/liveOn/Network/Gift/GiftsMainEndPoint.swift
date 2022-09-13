9ad519d1417274714485af3748e4d2aa45775516
Milli <jinccc72@pusan.ac.kr>
2022-09-13 19:03:42 +0900

    [Feat] Gifts exists api 연동

---
+++ liveOnReboot/liveOn/Network/Gift/GiftsMainEndPoint.swift
@@ -0,0 +1,53 @@
+//
+//  GiftsMainEndPoint.swift
+//  liveOnReboot
+//
+//  Created by Jineeee on 2022/09/13.
+//
+
+import Foundation
+import Moya
+
+extension GiftsMain: TargetType {
+
+    public var baseURL: URL {
+        return URL(string: GeneralAPI.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+            case .giftExists:
+                return "api/v1/gifts/main/gift-exists"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+            case .giftExists:
+                return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+            case .giftExists:
+                return .requestPlain
+        }
+    }
+    
+    var authorizationType: AuthorizationType? {
+        switch self {
+        default:
+            return .bearer
+        }
+    }
+
+    
+    var headers: [String : String]? {
+        switch self {
+            default:
+                return ["Content-Type": "application/json",
+                        "Authorization": "Bearer " + GeneralAPI.token]
+        }
+    }
+}
