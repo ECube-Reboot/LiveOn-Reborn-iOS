b7b4b35c13fc1967d258abbf81f7a765e2e6b5c1
Milli <jinccc72@pusan.ac.kr>
2022-09-15 23:09:03 +0900

    [Chore] 환경설정 UI 커스텀 폰트 삭제

--- liveOnReboot/liveOn/View/CoupleInformation/CoupleInformationView.swift
+++ liveOnReboot/liveOn/View/CoupleInformation/CoupleInformationView.swift
@@ -13,56 +13,50 @@ struct CoupleInformationView: View {
     @State private var showChangeOfficialDayAlert = false
     @State private var officialDate = Date.now
     var body: some View {
-                List {
-                    NavigationLink(destination: ChangeNickNameView()){
-                        HStack {
-                        Text("내 이름")
-                                .font(.TextStyles.handWrittenBody)
-                        Spacer()
-                            Text(MemberConfigService.singleton.profile.currentUserName)
-                                .font(.TextStyles.handWrittenCallout)
-                                .opacity(0.8)
-                        }
-                        .padding(.vertical, 12)
-                    }
-                    .foregroundColor(.textBodyColor)
-                    .listRowBackground(Color.clear)
-                    .listRowSeparator(.hidden)
-                    
-                    HStack {
-                        Text("상대")
-                                .font(.TextStyles.handWrittenBody)
-                        Spacer()
-                        Text(MemberConfigService.singleton.profile.partnerName)
-                                .font(.TextStyles.handWrittenCallout)
-                                .opacity(0.8)
-                    }
-                    .padding(.vertical, 12)
-                    .foregroundColor(.textBodyColor)
-                    .listRowBackground(Color.clear)
-                    .listRowSeparator(.hidden)
-//                    NavigationLink(destination: ChangeOfficialDateView(officialDay: $officialDate)){
-                        HStack {
-                        Text("우리의 1일")
-                                .font(.TextStyles.handWrittenBody)
-                        Spacer()
-                            Text(MemberConfigService.singleton.profile.officialDate)
-                                .font(.TextStyles.handWrittenCallout)
-                                .opacity(0.8)
-                        }
-                        .padding(.vertical, 12)
-//                    }
-                    .listRowBackground(Color.clear)
-                    .foregroundColor(.textBodyColor)
-                    .listRowSeparator(.hidden)
+        List {
+            NavigationLink(destination: ChangeNickNameView()){
+                HStack {
+                    Text("내 이름")
+                    Spacer()
+                    Text(MemberConfigService.singleton.profile.currentUserName)
+                        .font(.callout)
+                        .opacity(0.8)
                 }
+                .padding(.vertical, 12)
+            }
+            .listRowBackground(Color.clear)
+            .listRowSeparator(.hidden)
+            
+            HStack {
+                Text("상대")
+                Spacer()
+                Text(MemberConfigService.singleton.profile.partnerName)
+                    .font(.callout)
+                    .opacity(0.8)
+            }
+            .padding(.vertical, 12)
+            .listRowBackground(Color.clear)
+            .listRowSeparator(.hidden)
+            //                    NavigationLink(destination: ChangeOfficialDateView(officialDay: $officialDate)){
+            HStack {
+                Text("우리의 1일")
+                Spacer()
+                Text(MemberConfigService.singleton.profile.officialDate)
+                    .font(.callout)
+                    .opacity(0.8)
+            }
+            .padding(.vertical, 12)
+            //                    }
+            .listRowBackground(Color.clear)
+            .listRowSeparator(.hidden)
+        }
         .listRowBackground(Color.clear)
         .navigationTitle("커플정보")
         .navigationToBack(dismiss)
         .navigationBarTitleDisplayMode(.inline)
         .onAppear {
             MemberConfigService.fetchMemberProfile {
-                    
+                
             }
         }
         .toolbar {
