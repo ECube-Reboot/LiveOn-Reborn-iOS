6baee67c4d82426e54e676968100f652a3ba7995
Milli <jinccc72@pusan.ac.kr>
2022-10-18 19:38:17 +0900

    [Del] 스와이프로 뒤로가기 임시삭제

--- liveOnReboot/liveOn/Extensions/NavigationController+Extension.swift
+++ liveOnReboot/liveOn/Extensions/NavigationController+Extension.swift
@@ -1,19 +1,19 @@
+////
+////  NavigationController+Extension.swift
+////  liveOnReboot
+////
+////  Created by Jineeee on 2022/08/28.
+////
 //
-//  NavigationController+Extension.swift
-//  liveOnReboot
+//import Foundation
+//import SwiftUI
+//extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+//    override open func viewDidLoad() {
+//        super.viewDidLoad()
+//        interactivePopGestureRecognizer?.delegate = self
+//    }
 //
-//  Created by Jineeee on 2022/08/28.
-//
-
-import Foundation
-import SwiftUI
-extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        interactivePopGestureRecognizer?.delegate = self
-    }
-
-    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        return viewControllers.count > 1
-    }
-}
+//    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//        return viewControllers.count > 1
+//    }
+//}
